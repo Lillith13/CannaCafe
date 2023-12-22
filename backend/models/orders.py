51d76d5f687d48900199a02985ea65c4a4e7995b
Lillith13 <krystina.bell@outlook.com>
2023-12-21 21:24:46 -0500

    still working on it

--- backend/models/orders.py
+++ backend/models/orders.py
@@ -15,7 +15,7 @@ class Order(db.Model):
 
     total = db.Column(db.Numeric(precision=10, scale=2), default=0)
 
-    placed = db.Column(db.DateTime, default=datetime.datetime.utcnow(), nullable=False)
+    placed = db.Column(db.Date, default=datetime.datetime.utcnow(), nullable=False)
 
     shipped = db.Column(db.Date)
 
