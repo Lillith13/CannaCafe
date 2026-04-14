83723d77ec279c7fbc649ea5dc664c8b753e9bf9
Nina (Krystina Bell) <krystina.bell@outlook.com>
2026-04-14 12:38:11 -0400

    updated config file to accomodate simpler env file for easier startup

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,22 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+# This creates 'app.db' in the same folder as this script
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SECRET_KEY = '--- IGNORE --- ' if os.environ.get('DEBUG') else os.environ.get('SECRET_KEY')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') if os.environ.get('DATABASE_URL') else 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
